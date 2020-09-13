faace0863606036bc4e2fbd74175df95251e9901
TotalyAHuman <64377517+TotalyAHuman@users.noreply.github.com>
2020-09-13 11:57:42 +0200

    Add files via upload

---
+++ Python Cooking Database (SLO).py
@@ -0,0 +1,62 @@
+choice = input("Kaj hočeš speči? ")
+if choice == ("Banana cookies"):
+    print('''Potrebuješ:
+    -100g olja
+    -100g sladkorja (kokosovega)
+    -1 vrečka vanili sladkorja
+    -1 banana
+    -1 jajce
+    -220g polnozrnate pirine moke
+    -100g ovsenih kosmičev
+    -0,5 vrečke pecilnega praška
+    -1 ščepec soli
+    
+
+    Olje in oba sladkorja stepemo. Dodamo jajce in zmečkano banano. Dodamo še suhe sestavine in vse zmešamo z žlico. 
+
+    Na pekač, obložen s peki papirjem, polagamo kupčke mase. Med njimi pustimo nekaj centimetrov razmaka, saj se med peko malce razlezejo. Piškote pečemo 10-12 minut na 180 °C.
+    ''')
+if choice == ("Muffine"):
+    print('''Potrebuješ:
+    -250g moke
+    -150g sladkorja
+    -200g čokolade ali sadja
+    -1 vrečka pecilnega praška
+    -1 vrečka vanilijevega sladkorja
+    -2 jajci
+    -6 do 7 jedilnih žlic olja
+    -2dl mleka
+
+    
+    Vzamemo dve posodi in ločimo rumenjake od beljakov. Iz beljakov stepemo sneg. V posodo z 
+    rumenjaki dodamo sladkor, vanilijev sladkor in limonin sok ter pomešamo. Nato zmesi prilijemo olje. Premešamo.
+    Dodamo moko, ki vključuje vrečko pecilnega praška ter zmešamo. Dobimo suho maso, ki ji prilijemo mleko in mešamo toliko
+    časa, da dobimo gladko maso. Dodamo stepen sneg, ki smo ga pripravili na začetku. Dodamo 
+    še čokolado ali sadje (umešamo na roko, ne z aparatom!). Maso vlijemo v pekač za mafine, ki naj ne sega čez rob in pečemo 20 do 25 minut na 170 stopinjah.
+    ''')
+if choice == ("Palačinke"):
+    print('''Potrebuješ: 
+    -100g moke
+    -1 žličko sladkorja
+    -2 ščepca soli
+    -2 jajci
+    -300ml mleka
+    -1 žlico olja
+
+
+    V skledo stresemo moko. Dodamo soli in sladkor, dobro premešamo in na sredini naredimo jamico. V jamico
+    ubijemo jajci ter prilijemo približno 50 ml mleka in eno žlico olja.Sestavine v jamici pričnemo mešati
+    z ročno metlico ali mešalnikom in med mešanjem postopoma zajemamo moko okoli jamice. Mešamo tako dolgo,
+    da v zmes vmešamo vso moko (po potrebi prilijemo še malo mleka) ter dobimo gladko in gosto zmes brez grudic.
+    Nato med nenehnim mešanjem v tankem curku v skledo počasi vlivamo preostalo mleko. Ko prilijemo vse mleko in
+    dobimo gladko in tekoče testo, prenehamo z mešanjem. Masa mora biti tekoča, vendar ne sme biti vodena. Če je vodena, dodamo še malo moke. 
+    Pripravljeno maso pred peko pustimo počivati 15 do 30 minut, da lahko škrobna zrnca v tekočini nabreknejo. Pred peko jo obvezno še enkrat dobro premešamo.
+    ''')
+
+
+
+
+
+
+elif choice: print("Recepta ni v bazi podatkov")
+input("klikni ENTER za izhod")
